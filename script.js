f45918f1b2e759512ce33257640c3a659ac457c4
KatzRead <72603872+KatzRead@users.noreply.github.com>
2025-10-13 06:33:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -223,18 +223,23 @@ async function loadWidget(){
       const avgHS=(totalHS/matchCount).toFixed(1)+"%";
       const avgRow=document.createElement("tr");
       avgRow.classList.add("avg-row");
-      avgRow.innerHTML = `
+avgRow.innerHTML = `
 <td colspan="5" style="
     text-align:center;
-    font-weight:bold;
-    color: var(--theme-color);
-    text-shadow: 0 0 2px var(--theme-color);
-    border-top: 1px solid var(--theme-color);
+    font-weight:900;
+    font-size:1.05em;
+    color:#00ffff;
+    text-shadow:
+        0 0 4px #00ffff,
+        0 0 8px #00ffff,
+        0 0 12px #00ffff;
+    border-top: 1px solid #00ffff;
     padding-top:6px;
 ">
     AVG — ${avgKills} / ${avgDeaths} / ${avgKD} / ${avgADR} / ${avgHS}
 </td>
-`;    
+`;
+  
   }
 
   }catch{
