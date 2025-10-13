6b3ff0d4b404c28a108698f0866485bbc79dc0d4
KatzRead <72603872+KatzRead@users.noreply.github.com>
2025-10-13 06:33:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -234,7 +234,8 @@ async function loadWidget(){
 ">
     AVG — ${avgKills} / ${avgDeaths} / ${avgKD} / ${avgADR} / ${avgHS}
 </td>
-`;}
+`;    
+  }
 
   }catch{
     document.getElementById("elo").innerText="ELO alınamadı";
