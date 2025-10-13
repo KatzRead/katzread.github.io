f05a160c913fa2d2399aad93da9b4f448ecf60ac
KatzRead <72603872+KatzRead@users.noreply.github.com>
2025-10-13 05:44:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,23 @@ tabs.forEach(btn=>btn.addEventListener('click',()=>{
   document.getElementById(btn.dataset.tab).classList.add('active');
 }));
 
-// Crosshair
 const crosshair = document.getElementById("crosshair");
+
+// Crosshair hareketi
 document.addEventListener("mousemove", e => {
   crosshair.style.left = e.clientX + "px";
   crosshair.style.top = e.clientY + "px";
+
+  // Her move'da cursor'u gizle
+  document.body.style.cursor = "none";
+  document.querySelectorAll("button, input, select, a, textarea").forEach(el => {
+    el.style.cursor = "none";
+  });
 });
-/* Tüm tıklamaları ve sağ tıklamayı iptal et */
+
+// Sağ tıklamayı engelle
 document.addEventListener("contextmenu", e => e.preventDefault());
-["mousemove","mousedown","mouseup"].forEach(evt=>{document.addEventListener(evt,()=>{document.body.style.cursor="none";});});
+
 
 // Settings bar
 const settingsBar = document.getElementById("settings-bar");
